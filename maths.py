34795eb0a9068467ed75d0133b31aab126d3f198
ofabun <ofabun@ucsc.edu>
2026-04-06 16:55:59 -0700

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:44:40 2026
+
+@author: oliver
+"""
+w = 7.7
+x = 10
+y = 8
+z = 0.7
+
+
+# addition of two floating point numbers
+print("Addition of two floats:")
+print("w+z =", w+z,type(w+z))
+
+# difference between two integers
+print("Difference between two integers:")
+print("x-y = ", x-y, type(x-y))
+
+# product of a floating point number and an integer
+print("Product of a float & an integer:")
+print("x*z=", x*z, type(x*z))
+
