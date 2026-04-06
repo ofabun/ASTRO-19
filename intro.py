0c088836775ba41fbfae68944c4705bf447a407e
ofabun <ofabun@ucsc.edu>
2026-04-06 16:41:52 -0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:35:40 2026
+
+@author: oliver
+"""
+
+# Session 1 Prompt: Write a Python program to print out your full name, 
+# your preferred pronouns (optional), 
+# and two sentences about your favorite movie and your favorite food.
+print("Hello, my name is Oliver Fabun.")
+
+print("A favorite movie of mine would probably be Good Time.")
+print("I'm pretty big on cinematography --- that film's an impressive one.")
+
+print("My favorite food is probably just sushi, it's damn good.")
